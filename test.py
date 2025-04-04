2d19fce8c09e6f3885bb1869f44e957c1ad2bd09
Matthew Gracey <m.gracey1409@gmail.com>
2025-04-04 19:24:47 +0100

    Added really trash code

---
+++ test.py
@@ -0,0 +1,41 @@
+import cv2 as cv
+import numpy as np
+
+
+def main():
+    cam = cv.VideoCapture(0, cv.CAP_DSHOW)
+
+    lower_red1 = np.array([0, 120, 70])
+    upper_red1 = np.array([10, 255, 255])
+    lower_red2 = np.array([170, 120, 70])
+    upper_red2 = np.array([180, 255, 255])
+
+    while True:
+        success, img = cam.read()
+
+        gray_frame = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+        mask1 = cv.inRange(hsv, lower_red1, upper_red1)
+        mask2 = cv.inRange(hsv, lower_red2, upper_red2)
+        final = mask1 + mask2
+
+        contours, hierarchy = cv.findContours(final.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+        output = img.copy()
+        cv.drawContours(output, contours, -1, (0, 255, 0), 2)
+
+        cv.imshow("Test1", img)
+        cv.imshow("Test2", gray_frame)
+        cv.imshow("Test3", final)
+        cv.imshow("Test4", output)
+
+        key = cv.waitKey(10)
+
+        if key == "q":
+            break
+
+
+if __name__ == "__main__":
+    main()
