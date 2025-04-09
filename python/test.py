cfd297609c925ef9c62804e7873794b821cb0b25
Matthew Gracey <m.gracey1409@gmail.com>
2025-04-09 12:32:22 +0100

    Added a whole bunch of images to destroy Microsofts free cloud storage

--- python/test.py
+++ python/test.py
@@ -1,13 +1,13 @@
 import cv2 as cv
 import numpy as np
 
+import ./pipeline
+
 def main():
     cam = cv.VideoCapture(0, cv.CAP_DSHOW)
 
-    lower_red1 = np.array([0, 120, 70])
-    upper_red1 = np.array([10, 255, 255])
-    lower_red2 = np.array([170, 120, 70])
-    upper_red2 = np.array([180, 255, 255])
+    lower_red = np.array([0, 120, 70])
+    upper_red = np.array([10, 255, 255])
 
     # lower_blue = np.array([100, 100, 80])
     # upper_blue = np.array([140, 255, 255])
@@ -19,9 +19,7 @@ def main():
 
         hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-        mask1 = cv.inRange(hsv, lower_red1, upper_red1)
-        mask2 = cv.inRange(hsv, lower_red2, upper_red2)
-        final = mask1 + mask2
+        mask = cv.inRange(hsv, lower_red, upper_red)
 
         # mask1 = cv.inRange(hsv, lower_blue, upper_blue)
         # final = mask1
@@ -40,6 +38,10 @@ def main():
 
         if key == ord('q'):
             break
+
+
+def getBoundingRectangle(lower, upper):
+    pass
 
 
 if __name__ == "__main__":
