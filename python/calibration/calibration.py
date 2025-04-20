07099267ebd2a1f12d4e8a0b7bf442d993fe3de4
Matthew Gracey <m.gracey1409@gmail.com>
2025-04-20 21:14:03 +0100

    Calibration complete

--- python/calibration/calibration.py
+++ python/calibration/calibration.py
@@ -1,5 +1,3 @@
-# Direct copy from the OpenCv docs
-
 import numpy as np
 import cv2 as cv
 import glob
@@ -8,8 +6,8 @@ import glob
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((8*5,3), np.float32)
+objp[:,:2] = np.mgrid[0:5,0:8].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -22,7 +20,7 @@ for fname in images:
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv.findChessboardCorners(gray, (5,8), None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -32,8 +30,30 @@ for fname in images:
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.drawChessboardCorners(img, (5,8), corners2, ret)
         cv.imshow('img', img)
-        cv.waitKey(500)
+        cv.waitKey(100)
 
 cv.destroyAllWindows()
+
+#print(f'Object points: {objpoints}')
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+for fname in images:
+        img = cv.imread(fname)
+        h,  w = img.shape[:2]
+        newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+        # undistort
+        mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+        dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+	
+        # crop the image
+        x, y, w, h = roi
+        dst = dst[y:y+h, x:x+w]
+
+        cv.imshow('img', img)
+        cv.imshow('dst', dst)
+        cv.waitKey(2000)
+
